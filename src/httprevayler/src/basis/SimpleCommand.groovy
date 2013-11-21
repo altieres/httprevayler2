1a8a579d542b07287687f1da694968d68b3c2117
Altieres <altiereslopes@webgoal.com.br>
2013-11-20 22:20:34 -0200

    fix concurrent get override

--- src/httprevayler/src/basis/SimpleCommand.groovy
+++ src/httprevayler/src/basis/SimpleCommand.groovy
@@ -1,22 +1,21 @@
 package httprevayler.src.basis
 
-import java.io.Serializable;
 
 class SimpleCommand implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private final SimpleRequest request
-	private static SimpleResponse response = new SimpleResponseDummy()
+	private static final ThreadLocal<SimpleResponse> response = new ThreadLocal<SimpleResponse>()
 	
 	public SimpleCommand(SimpleRequest request, SimpleResponse response) {
 		this.request = request
-		this.response = response
+		this.response.set(response)
 	}
 	
 	protected Object process(Object application, Date date) {
 		request.parsedMap['now'] = date
-		ApplicationServletFactory.create().service(request, response)
+		ApplicationServletFactory.create().service(request, response.get() == null ? new SimpleResponseDummy() : response.get())
 	}
 
 }
