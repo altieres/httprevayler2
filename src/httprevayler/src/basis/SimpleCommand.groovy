885e53b432505b40190163eddaf6c3d9a995f513
Altieres <altiereslopes@webgoal.com.br>
2013-11-21 10:57:57 -0200

    response by command instead of by thread (ThreadLocal)

--- src/httprevayler/src/basis/SimpleCommand.groovy
+++ src/httprevayler/src/basis/SimpleCommand.groovy
@@ -1,21 +1,28 @@
 package httprevayler.src.basis
 
+import java.util.concurrent.atomic.AtomicLong
+
 
 class SimpleCommand implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private final SimpleRequest request
-	private static final ThreadLocal<SimpleResponse> response = new ThreadLocal<SimpleResponse>()
+	
+	private final long responseId
+	private static AtomicLong nextResponseId = new AtomicLong();
+	private static final Map<Long, SimpleResponse> responsesById = new HashMap<Long, SimpleResponse>()
 	
 	public SimpleCommand(SimpleRequest request, SimpleResponse response) {
 		this.request = request
-		this.response.set(response)
+		responseId = nextResponseId.incrementAndGet()
+		this.responsesById.put(responseId, response)
 	}
 	
 	protected Object process(Object application, Date date) {
 		request.parsedMap['now'] = date
-		ApplicationServletFactory.create().service(request, response.get() == null ? new SimpleResponseDummy() : response.get())
+		SimpleResponse response = responsesById.remove(responseId)
+		ApplicationServletFactory.create().service(request, response == null ? new SimpleResponseDummy() : response)
 	}
 
 }
